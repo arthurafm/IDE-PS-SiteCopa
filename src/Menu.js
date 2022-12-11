d2aaaf663386e32bb07ba5b878eea56b9a1c04cc
Arthur <arthuralvesfmelo@gmail.com>
2022-12-11 19:10:21 -0300

    Header feito

---
+++ src/Menu.js
@@ -0,0 +1,17 @@
+import './Menu.css'
+
+function Menu() {
+    return (
+        <nav className="Menu">
+            <ul className="Menu--options">
+                <li>HOME</li>
+                <li>SOBRE</li>
+                <li>JOGADORES</li>
+                <li>JOGOS</li>
+            </ul>
+        </nav>
+    )
+}
+
+export default Menu
+
