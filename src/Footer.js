45459aff8e15bafeea8716e2696bf9e81692bcbd
Arthur <arthuralvesfmelo@gmail.com>
2022-12-12 17:41:54 -0300

    Código documentado para Footer()

--- src/Footer.js
+++ src/Footer.js
@@ -2,10 +2,11 @@ import './Footer.css'
 import IDE from './images/IDE-logo.png'
 import CBF from './images/CBF-logo.png'
 
+// Rodapé em formato de elementos concatenados
 function Footer() {
     return (
         <footer className='Footer'>
-            <img className="CBF--logo"src={CBF} alt="Logo da CBF."/>
+            <img className="CBF--logo" src={CBF} alt="Logo da CBF."/>
             <small className='Footer--text'>Confederação Brasileira de Futebol © x IDE © Todos os direitos reservados.<br /><br />Termos de Uso | Politica de Privacidade | Política de Cookies</small>
             <div className='Footer--RightSide'>
                 <small className='Footer--text'>Realização:</small>
