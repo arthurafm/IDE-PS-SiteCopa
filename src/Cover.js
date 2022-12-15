6d62e50ceca6242e9a216777e42de226b237001d
Bruno Bour <117775202+BrunoBour@users.noreply.github.com>
2022-12-14 23:26:05 -0300

    Add files via upload

--- src/Cover.js
+++ src/Cover.js
@@ -1,7 +1,12 @@
 import './Cover.css'
 
+// Div do Cover estilizada no Cover.css
 function Cover() {
-    
+    return(
+    <div className='Cover' >
+            <h1 className='CoverTitle'>Seleção Brasileira <br/>de Futebol</h1>
+    </div>
+    )
 }
 
 export default Cover
