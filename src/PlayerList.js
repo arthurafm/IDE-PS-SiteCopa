d638848b16c22af99b3f35af3b78b2a28914e0d3
Andre-klarmann <112667579+Andre-klarmann@users.noreply.github.com>
2022-12-17 12:13:56 -0300

    playerlist adicionada

--- src/PlayerList.js
+++ src/PlayerList.js
@@ -1,7 +1,114 @@
-import './PlayerList.css'
+import { useState } from "react";
+import "./PlayerList.css";
 
-function PlayerList() {
-    
-}
+const atacantes = [
+	{ name: "Jogador 1", age: 27 },
+	{ name: "Jogador 2", age: 31 },
+	{ name: "Jogador 3", age: 25 },
+	{ name: "Jogador 4", age: 34 },
+	{ name: "Jogador 5", age: 19 },
+	{ name: "Jogador 6", age: 34 },
+	{ name: "Jogador 7", age: 25 },
+	{ name: "Jogador 8", age: 34 },
+	{ name: "Jogador 9", age: 25 },
+];
 
-export default PlayerList
+const meioCampo = [
+	{ name: "Jogador 10", age: 27 },
+	{ name: "Jogador 11", age: 31 },
+	{ name: "Jogador 12", age: 25 },
+	{ name: "Jogador 13", age: 39 },
+	{ name: "Jogador 14", age: 19 },
+	{ name: "Jogador 15", age: 84 },
+];
+
+const defensores = [
+	{ name: "Jogador 16", age: 27 },
+	{ name: "Jogador 17", age: 31 },
+	{ name: "Jogador 18", age: 25 },
+	{ name: "Jogador 19", age: 34 },
+	{ name: "Jogador 20", age: 19 },
+	{ name: "Jogador 21", age: 34 },
+	{ name: "Jogador 22", age: 25 },
+	{ name: "Jogador 23", age: 34 },
+];
+
+const goleiros = [
+	{ name: "Jogador 24", age: 27 },
+	{ name: "Jogador 25", age: 31 },
+	{ name: "Jogador 26", age: 25 },
+];
+
+const PlayerComponent = (props) => {
+	return (
+		<div className="container">
+			<div className="player-info">
+				<div className="photo"></div>
+				<div>
+					<h2>{props.name}</h2>
+					<p>Idade: {props.age}</p>
+				</div>
+			</div>
+			<h1 className="Symbol">J</h1>
+		</div>
+	);
+};
+
+const PlayerList = (props) => {
+	const [numPlayer, setnumPlayer] = useState(atacantes);
+
+	return (
+		<div className="PlayerList">
+			<div id="Rectangle">
+				<h1 className="AboutTitle">{props.title}</h1>
+			</div>
+			<nav className="Menu1">
+				<ul className="MenuItens">
+					<li
+						onClick={(e) => {
+							setnumPlayer(atacantes);
+						}}
+						value="Atacantes"
+						className="itens active"
+					>
+						Atacantes
+					</li>
+					<li
+						onClick={(e) => {
+							setnumPlayer(meioCampo);
+						}}
+						value="Meias"
+						className="itens"
+					>
+						Meio Campo
+					</li>
+					<li
+						onClick={(e) => {
+							setnumPlayer(defensores);
+						}}
+						value="Defesa"
+						className="itens"
+					>
+						Defensores
+					</li>
+					<li
+						onClick={(e) => {
+							setnumPlayer(goleiros);
+						}}
+						value="Gol"
+						className="itens"
+					>
+						Goleiros
+					</li>
+				</ul>
+			</nav>
+			<div className="players">
+				{numPlayer.map((player) => {
+					return <PlayerComponent name={player.name} age={player.age} />;
+				})}
+			</div>
+		</div>
+	);
+};
+
+export default PlayerList;
