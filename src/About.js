cfa71ad72a47a3c13b44ab6a70e2b8ab5a3d0d19
Bruno Bour <117775202+BrunoBour@users.noreply.github.com>
2022-12-16 22:09:32 -0300

    Cover sem Logos, remover comentários atípicos

--- src/About.js
+++ src/About.js
@@ -6,7 +6,7 @@ function About() {
     <div className='About' id="About">
         <div id='Rectangle'><h1 className='AboutTitle'>SOBRE A SELEÇÃO</h1></div>
         
-        <p>
+        <p id='PrimaryText'>
              A Seleção Brasileira de Futebol representa o brasil nas competições de futebol da CONMEBOL e FIFA. <br /> <br />
 
             Formada em 1915 e considerada um dos maiores símbolos do país, é chamada de "Seleção",
@@ -16,11 +16,11 @@ function About() {
             
             <div className='RectAux'>
             <div className='Box'>
-            <h2>Tem 5 títulos invictos (1958, 1962, 1970, 1994 e 2002) e quatro títulos da Copa das Confederações
+            <h2 id='BoxTxt'>Tem 5 títulos invictos (1958, 1962, 1970, 1994 e 2002) e quatro títulos da Copa das Confederações
             FIFA (1997, 2005, 2009 e 2013)</h2>
             </div>
             </div>
-        <img src={arquibancada} alt="arquibancada com bandeira do brasil"/>
+        <img src={arquibancada} id="AboutImage" alt="arquibancada com bandeira do brasil"/>
 
         <p id='SubTitle'>
              MAIS QUE 5 ESTRELAS, 200 MILHÕES DE CORAÇÕES <br />
