122c3b74a4d949f61c566096a3ffa6bd241cf3dc
Bruno Bour <117775202+BrunoBour@users.noreply.github.com>
2022-12-16 22:50:51 -0300

    Logos terminados + documentado

--- src/Cover.js
+++ src/Cover.js
@@ -7,6 +7,11 @@ import CBF from "./images/CBF-logo.png"
 function Cover() {
     return(
     <div className='Cover'  id="Home">
+           <div className='CoverLogos'>
+                <img src={ IDE } className="LogoIDE" alt="logo da IDE" />
+                <p className='VersusSymbol'>X</p>
+                <img src= { CBF } className="LogoBrazil" alt="logo da seleção brasileira" />
+            </div> 
             <h1 className='CoverTitle'>Seleção Brasileira <br/>de Futebol</h1>
     </div>
     
